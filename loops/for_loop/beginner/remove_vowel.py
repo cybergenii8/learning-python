5a10e155dc0dfdb4aa86d241360ce6eb09e2bf26
Bagas Setia <bagassb2001@gmail.com>
2025-05-16 13:56:42 +0700

    Add remove vowel file

---
+++ loops/for_loop/beginner/remove_vowel.py
@@ -0,0 +1,19 @@
+# Remove Vowels from String
+
+# Task: Write a python program that takes a string input from the user and removes all vowels using a for loop.
+
+# Get user input
+user_string = input("Enter a text: ")
+
+# Initialize an empty variable to store text after remove all vowel
+removed_vowel = ""
+
+# Initialize a variable to store vowel
+vowel = "aiueoAIUEO"
+
+# Use for loop to remove vowel
+for char in user_string:
+    if not char in vowel:
+        removed_vowel += char
+
+print(f"{user_string} after the vowel is removed: {removed_vowel}")
