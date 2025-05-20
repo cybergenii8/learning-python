5c52c8c02ab81a0bb97798448b9f4c06158fd321
Bagas Setia <bagassb2001@gmail.com>
2025-05-20 10:41:54 +0700

    Add countdown timer file

---
+++ functions/countdown_timer.py
@@ -0,0 +1,10 @@
+# Countdown Timer
+
+# Task: Write a function countdown(n) that prints numbers from n to 0 using a while loop.
+
+def countdown(n):
+    while n >= 0:
+        print(n)
+        n -= 1
+
+countdown(10)
